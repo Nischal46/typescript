56cc782f5a79fb71c07c1c01f9fdb9fcffb54a33
Nischal <baniyanisal12@gmail.com>
2024-06-27 20:56:52 +0545

    ignoring node modules files and also implements the logic of the json web token

--- src/Practice4.ts
+++ src/Practice4.ts
@@ -1,4 +1,5 @@
 const bcrypting = require("bcryptjs");
+const jwt = require('jsonwebtoken');
 
 let PasswordStoreDB: unknown;
 async function PasswordEncrypter(pass: string): Promise<string> {
@@ -6,14 +7,14 @@ async function PasswordEncrypter(pass: string): Promise<string> {
     return encryptPass;
 }
 
-PasswordEncrypter('hello')
-    .then((res) => {
-        PasswordStoreDB = <string>res
-        console.log(PasswordStoreDB);
-        PasswordChecker('hello');
-    })
-    .catch((err) => console.log(err)
-    )
+// PasswordEncrypter('hello')
+//     .then((res) => {
+//         PasswordStoreDB = <string>res
+//         console.log(PasswordStoreDB);
+//         PasswordChecker('hello');
+//     })
+//     .catch((err) => console.log(err)
+//     )
 
 async function PasswordChecker(pass: string) {
 
@@ -24,6 +25,33 @@ async function PasswordChecker(pass: string) {
     else console.log('Password doesnot match');
 
 }
+
+let secretjwt = '3423dsfdsfs'
+let token: unknown;
+
+function JWTProvider(payload: string) : string{
+    token = <string>jwt.sign({payload}, secretjwt, {expiresIn: '1h'});
+    console.log(token);
+    
+    return <string>token;
+}
+
+function verifyToken(): void{
+    let jwtcheckin: unknown;
+    try {
+        jwtcheckin = <boolean>jwt.verify(token, secretjwt);
+        console.log('jwt successfully verified');
+    } catch (error) {
+        console.log(error);
+        console.log('failed to verify jwt token');
+    }
+}
+
+JWTProvider('jwtsender');
+verifyToken();
+
+
+
 
 
 
