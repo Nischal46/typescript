a6aaef293fef569b61ff0c338f676057da7ce9f9
Nischal <baniyanisal12@gmail.com>
2024-07-14 21:42:23 +0545

    defining interface and its use cases

--- src/Practice10.ts
+++ src/Practice10.ts
@@ -32,3 +32,14 @@ let emp: Person = new Employee("James", 100);
 emp.display(); //James
 
 let emp2: Person = emp.find('Steve');
+
+interface KeyPair {
+    key: number;
+    value: string;
+}
+
+let kv1: KeyPair = { key:1, value:"Steve" }; // OK
+
+// let kv2: KeyPair = { key:1, val:"Steve" }; // Compiler Error: 'val' doesn't exist in type 'KeyPair'
+
+// let kv3: KeyPair = { key:1, value:100 }; // Compiler Error: 
