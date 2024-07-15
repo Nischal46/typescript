2ed570306b9c994ee8eb51064531ca93a457d9d5
Nischal <baniyanisal12@gmail.com>
2024-07-15 22:40:26 +0545

    generics used in function

---
+++ src/Practice11.ts
@@ -0,0 +1,22 @@
+//Generics in typescript : used for the reusable of the code
+
+function getArrayDetails<T>(items: T[]){
+    return new Array<T>().concat(items)
+}
+
+let numArray = getArrayDetails<number>([12,34,54,65,77,87]);
+console.log('The number array detail is ', numArray);
+
+let stringArray = getArrayDetails<string>(['nischal', 'baniya']);
+console.log('The string array detail is ', stringArray);
+
+//Accepting multiple generics
+
+function DisplayDetails<T, U>(name: T, contact: U){
+    return {name, contact}
+}
+
+let displayDetails = DisplayDetails<string, number>('Nischal Baniya', 9832343223);
+console.log(displayDetails);
+
+
