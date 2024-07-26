80f1143e5115c9acbd10ee0f70297de73355afe0
Nischal <baniyanisal12@gmail.com>
2024-07-26 22:35:29 +0545

    abstract class inheriting

--- src/Practice17.ts
+++ src/Practice17.ts
@@ -34,3 +34,36 @@ async function mainfn(){
 }
 
 mainfn();
+
+abstract class Person {
+    name: string;
+    
+    constructor(name: string) {
+        this.name = name;
+    }
+
+    display(): void{
+        console.log(this.name);
+    }
+
+    abstract find(string): Person;
+}
+
+class Employee extends Person { 
+    empCode: number;
+    
+    constructor(name: string, code: number) { 
+        super(name); 
+        this.empCode = code;
+    }
+
+    find(name:string): Person { 
+       
+        return new Employee(name, 1);
+    }
+}
+
+let emp: Person = new Employee("James", 100);
+emp.display();
+
+let emp2: Person = emp.find('Steve');
