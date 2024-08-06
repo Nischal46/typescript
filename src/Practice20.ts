1c168e08dfe110f0daf4614bb81a8bb2d94f3e99
Nischal <baniyanisal12@gmail.com>
2024-08-06 23:24:18 +0545

    class and object

---
+++ src/Practice20.ts
@@ -0,0 +1,39 @@
+interface User {
+    name: string;
+    email: string;
+    password: string;
+}
+
+class UserManagement {
+    private users: User[] = [];
+
+    async CreateUser(name: string, email: string, password: string): Promise<User> {
+        const newUser: User = { name, email, password };
+        this.users.push(newUser);
+        return newUser;
+    }
+
+    async LoginUser(email: string, password: string): Promise<boolean> {
+        const user = this.users.find(user => user.email === email && user.password === password);
+        return user !== undefined;
+    }
+
+    async GetAllUser(): Promise<User[]> {
+        return this.users;
+    }
+
+    async createDocument() {
+        const user = await this.CreateUser('nischal', 'nishal@node.js', 'admin');
+        console.log('User created:', user);
+    }
+}
+
+// Example usage:
+(async () => {
+    const userManager = new UserManagement();
+    await userManager.createDocument();
+    const users = await userManager.GetAllUser();
+    console.log('All users:', users);
+    const isLoggedIn = await userManager.LoginUser('nishal@node.js', 'admin');
+    console.log('Login successful:', isLoggedIn);
+})();
