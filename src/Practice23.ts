3c8061ad2e03481c85c0a2178613759abb5e4b83
Nischal <baniyanisal12@gmail.com>
2024-08-21 21:49:56 +0545

    letter counting

--- src/Practice23.ts
+++ src/Practice23.ts
@@ -90,3 +90,40 @@ RegisterUserData(userDetails1).then((res) => console.log(res)
 )
 
 
+
+let sentence = "mouse and keyboard";
+
+
+let letterCounting = <any>{};
+
+
+function Counting(sentence:string) {
+    for (const x of sentence) {
+        if (x !== ' ') {
+            if (!letterCounting[x]) {
+                letterCounting[x] = 1;
+            }
+
+            else {
+                letterCounting[x] += 1;
+            }
+        }
+    }
+
+    console.log(letterCounting);
+
+}
+
+Counting(sentence);
+// Counting([1,1,1,2,3,1,4])
+
+
+const nestedArray = [12, 43, 23, [23, 42, 54, 65, [47, [90], 65]]];
+const weakMapconcept = nestedArray.flatMap((cl) => cl);
+
+console.log(weakMapconcept);
+
+const flatmap = nestedArray.flat(Infinity);
+console.log(flatmap);
+
+
