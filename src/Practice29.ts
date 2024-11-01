65632741201bf929009f1be5de2096cc1c86942a
Nischal46 <baniyanisal122gmail.com>
2024-11-01 23:50:26 +0545

    concept of enum type in ts

---
+++ src/Practice29.ts
@@ -0,0 +1,15 @@
+{
+    //enum concept
+
+    enum Color{
+        red, green, blue
+    }
+
+    let color1: Color = Color.blue;
+
+    if(color1 === Color.blue) console.log('Yes it is blue color');
+
+    else console.log('No it is not blue color');
+    
+    
+}
